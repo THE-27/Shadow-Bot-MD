98e35a639171dd1ebd4361c708b30d555c628a5e
THE-27 <blackoficial2025@gmail.com>
2025-05-07 23:49:49 -0500

    Actualizo main-menu-js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -12,13 +12,16 @@ let handler = async (m, { conn, usedPrefix, __dirname }) => {
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
     let totalreg = Object.keys(global.db.data.users).length
-    let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://qu.ax/Wksxu.jpg')
+    let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://pin.it/6FyGBp4dQ')
     let taguser = '@' + userId.split("@s.whatsapp.net")[0]
 
     let images = [
-      'https://qu.ax/XPSSg.jpg',
-      'https://qu.ax/Wksxu.jpg',
-      'https://qu.ax/jXVAs.jpg'
+      'https://pin.it/3izek4k9l',
+      'https://pin.it/5iS8bn0fp',
+      'https://pin.it/5U8hyutUW',
+      'https://pin.it/6DUG0HRE2',
+      'https://pin.it/52TcCHsn9',
+      'https://pin.it/2mJcg3qA3'
     ]
     let randomImage = images[Math.floor(Math.random() * images.length)]  
 
@@ -95,6 +98,7 @@ let handler = async (m, { conn, usedPrefix, __dirname }) => {
 ∫⚔️✎ .google <búsqueda>
 ∫⚔️✎ .imagen <query>
 ∫⚔️✎ .infoanime
+∫⚔️✎ .githubstalk <query>
 ∫⚔️✎ .soundcloudsearch <txt>
 ∫⚔️✎ .pinterest
 ∫⚔️✎ .pornhubsearch
